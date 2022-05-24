164014162dd15940fd90a5074a86bc0ff9dbd1b9
Lu Zhang <lu.zhang@ucla.edu>
2022-05-24 09:25:11 -0400

    modify the choice of hyperparmeters and clean up the code

--- sim/projects/check_z_var.R
+++ sim/projects/check_z_var.R
@@ -2,60 +2,8 @@ library(geoR)
 library(rdist)
 library(ggplot2)
 rm(list = ls())
-c_p1 <- c()
-samplesize_ls  = c(100, 200, 400, 800, 1600)
-N_ls <- length(samplesize_ls)
-d = 2
 
-for(i in 1:N_ls){
-  N = samplesize_ls[i]
-  if (d == 1){
-    coords <- runif(N)
-  }else{coords <- cbind(runif(N), runif(N))}
-  D <- as.matrix(dist(coords))
-  phi = 6.0; nu = 0.5
-  R <- matern(D, phi = 1/phi, kappa = nu)
-  if(d == 1){
-    s0 = runif(1)
-  }else{
-    s0 = runif(2)
-  }
-  U = matern(cdist(t(s0), coords), phi = 1/phi, kappa = nu)
-  cholR = chol(R)
-  deltasq = 0.2
-  p1 <- 1 + U %*% (solve(diag(N) +  deltasq^{-1}*R) - diag(N)) %*% 
-    chol2inv(cholR) %*% t(U)
-  c_p1[i] <- p1
-  dd <- diag(solve(deltasq^{-1}*diag(N) + chol2inv(cholR)))
-  
-  if (interactive()) {
-    if(d==1){
-      hist(dd, xlab = "", main = "", xlim = c(0, 0.3))
-    }else{
-      hist(dd, xlab = "", main = "", xlim = c(0, 0.5))
-    }
-    print(c(range(dd), max(dd)/min(dd)))
-    print(p1)
-    user_inp = readline(prompt="Press [enter] to continue or enter q to quit: ")
-    if (user_inp == "q") {
-      break
-    } else if (user_inp == "browse") {
-      browser()
-    }
-  } else {
-    print("Idk how to show graphs with Rscript :-(")
-    break
-  }
-}
-
-if(d == 1){
-  plot(log(samplesize_ls), c_p1, ylim = c(0, 0.3))
-}else{
-  plot(log(samplesize_ls), c_p1, ylim = c(0, 0.5))
-}
-
-
-## experimental analysis for the posterior variance E2 ##
+## experimental analysis of the posterior variance E2 ##
 library(fields)
 1 / Matern.cor.to.range(0.5, 0.5, cor.target=.05) # 6.0
 1 / Matern.cor.to.range(0.5, 1.0, cor.target=.05) # 8.0
@@ -85,6 +33,7 @@ for(i in 1:N_ls){
     deltasq = 1.0
     R <- matern(D, phi = 1/phi, kappa = nu)
     cholR = chol(R)
+    # diagonal elements of the covariance of posterior distribution of z
     pos_var_z_d1[[i]][, j] <- diag(solve(deltasq^{-1}*diag(N) + chol2inv(cholR)))
   }
 }
@@ -105,8 +54,8 @@ E2_d1_med_dat$N_sample <- factor(E2_d1_med_dat$N_sample, levels = 1:N_ls,
                                  labels = paste(samplesize_ls))
 E2_d1_med_dat$label <- factor(E2_d1_med_dat$label, levels = 1:N_psi,
                               labels = c("phi=6.0, nu=0.5, deltasq=1.0",
-                                         "phi=9.0, nu=1.0, deltasq=1.0",
-                                         "phi=12.0, nu=1.5, deltasq=1.0"))
+                                         "phi=8.0, nu=1.0, deltasq=1.0",
+                                         "phi=9.5, nu=1.5, deltasq=1.0"))
 
 p1_1 <- ggplot(data = E2_d1_med_dat, 
                aes(x = N_sample, y = median, group = label)) + 
@@ -121,23 +70,23 @@ p1_1 <- ggplot(data = E2_d1_med_dat,
   xlab("sample size") + ylab("variance of z") + 
   scale_shape_discrete(
     name  ="",
-    breaks=c("phi=6.0, nu=0.5, deltasq=1.0", "phi=9.0, nu=1.0, deltasq=1.0",
-             "phi=12.0, nu=1.5, deltasq=1.0"),
+    breaks=c("phi=6.0, nu=0.5, deltasq=1.0", "phi=8.0, nu=1.0, deltasq=1.0",
+             "phi=9.5, nu=1.5, deltasq=1.0"),
     labels=c(expression(phi ~ "="~ 6.0 ~ "," ~ nu ~ "=" ~ 0.5 ~ 
                           "," ~ delta^2 ~ "=" ~ 1.0), 
-             expression(phi ~ "="~ 9.0 ~ "," ~ nu ~ "=" ~ 1.0 ~ 
+             expression(phi ~ "="~ 8.0 ~ "," ~ nu ~ "=" ~ 1.0 ~ 
                           "," ~ delta^2 ~ "=" ~ 1.0),
-             expression(phi ~ "="~ 12.0 ~ "," ~ nu ~ "=" ~ 1.5 ~ 
+             expression(phi ~ "="~ 9.5 ~ "," ~ nu ~ "=" ~ 1.5 ~ 
                           "," ~ delta^2 ~ "=" ~ 1.0))) +
   scale_linetype_discrete(
     name  ="",
-    breaks=c("phi=6.0, nu=0.5, deltasq=1.0", "phi=9.0, nu=1.0, deltasq=1.0",
-             "phi=12.0, nu=1.5, deltasq=1.0"),
+    breaks=c("phi=6.0, nu=0.5, deltasq=1.0", "phi=8.0, nu=1.0, deltasq=1.0",
+             "phi=9.5, nu=1.5, deltasq=1.0"),
     labels=c(expression(phi ~ "="~ 6.0 ~ "," ~ nu ~ "=" ~ 0.5 ~ 
                           "," ~ delta^2 ~ "=" ~ 1.0), 
-             expression(phi ~ "="~ 9.0 ~ "," ~ nu ~ "=" ~ 1.0 ~ 
+             expression(phi ~ "="~ 8.0 ~ "," ~ nu ~ "=" ~ 1.0 ~ 
                           "," ~ delta^2 ~ "=" ~ 1.0),
-             expression(phi ~ "="~ 12.0 ~ "," ~ nu ~ "=" ~ 1.5 ~ 
+             expression(phi ~ "="~ 9.5 ~ "," ~ nu ~ "=" ~ 1.5 ~ 
                           "," ~ delta^2 ~ "=" ~ 1.0))) 
 p1_1
 ggsave(paste0("./sim/pics/check_z_var_d1.png"), 
@@ -150,7 +99,7 @@ ggsave(paste0("./sim/pics/check_z_var_d1.png"),
 samplesize_ls  = c(100, 300, 900, 2700, 8100, 24300)
 N_ls <- length(samplesize_ls)
 pos_var_z_d2 = list()
-phi_ls = c(6.0, 9.0, 12.0)
+phi_ls = c(6.0, 8.0, 9.5)
 nu_ls = c(0.5, 1.0, 1.5)
 N_psi = length(phi_ls)
 
@@ -170,6 +119,7 @@ for(i in 1:N_ls){
     deltasq = 1.0
     R <- matern(D, phi = 1/phi, kappa = nu)
     cholR = chol(R)
+    # diagonal elements of the covariance of posterior distribution of z
     pos_var_z_d2[[i]][, j] <- diag(solve(deltasq^{-1}*diag(N) + chol2inv(cholR)))
   }
 }
@@ -192,8 +142,8 @@ E2_d2_med_dat$N_sample <- factor(E2_d2_med_dat$N_sample, levels = 1:N_ls,
                              labels = paste(samplesize_ls))
 E2_d2_med_dat$label <- factor(E2_d2_med_dat$label, levels = 1:N_psi,
                           labels = c("phi=6.0, nu=0.5, deltasq=1.0",
-                                     "phi=9.0, nu=1.0, deltasq=1.0",
-                                     "phi=12.0, nu=1.5, deltasq=1.0"))
+                                     "phi=8.0, nu=1.0, deltasq=1.0",
+                                     "phi=9.5, nu=1.5, deltasq=1.0"))
 
 p1_2 <- ggplot(data = E2_d2_med_dat, 
                aes(x = N_sample, y = median, group = label)) + 
@@ -208,23 +158,23 @@ p1_2 <- ggplot(data = E2_d2_med_dat,
   xlab("sample size") + ylab("variance of z") + 
   scale_shape_discrete(
     name  ="",
-    breaks=c("phi=6.0, nu=0.5, deltasq=1.0", "phi=9.0, nu=1.0, deltasq=1.0",
-             "phi=12.0, nu=1.5, deltasq=1.0"),
+    breaks=c("phi=6.0, nu=0.5, deltasq=1.0", "phi=8.0, nu=1.0, deltasq=1.0",
+             "phi=9.5, nu=1.5, deltasq=1.0"),
     labels=c(expression(phi ~ "="~ 6.0 ~ "," ~ nu ~ "=" ~ 0.5 ~ 
                           "," ~ delta^2 ~ "=" ~ 1.0), 
-             expression(phi ~ "="~ 9.0 ~ "," ~ nu ~ "=" ~ 1.0 ~ 
+             expression(phi ~ "="~ 8.0 ~ "," ~ nu ~ "=" ~ 1.0 ~ 
                           "," ~ delta^2 ~ "=" ~ 1.0),
-             expression(phi ~ "="~ 12.0 ~ "," ~ nu ~ "=" ~ 1.5 ~ 
+             expression(phi ~ "="~ 9.5 ~ "," ~ nu ~ "=" ~ 1.5 ~ 
                           "," ~ delta^2 ~ "=" ~ 1.0))) +
   scale_linetype_discrete(
     name  ="",
-    breaks=c("phi=6.0, nu=0.5, deltasq=1.0", "phi=9.0, nu=1.0, deltasq=1.0",
-             "phi=12.0, nu=1.5, deltasq=1.0"),
+    breaks=c("phi=6.0, nu=0.5, deltasq=1.0", "phi=8.0, nu=1.0, deltasq=1.0",
+             "phi=9.5, nu=1.5, deltasq=1.0"),
     labels=c(expression(phi ~ "="~ 6.0 ~ "," ~ nu ~ "=" ~ 0.5 ~ 
                           "," ~ delta^2 ~ "=" ~ 1.0), 
-             expression(phi ~ "="~ 9.0 ~ "," ~ nu ~ "=" ~ 1.0 ~ 
+             expression(phi ~ "="~ 8.0 ~ "," ~ nu ~ "=" ~ 1.0 ~ 
                           "," ~ delta^2 ~ "=" ~ 1.0),
-             expression(phi ~ "="~ 12.0 ~ "," ~ nu ~ "=" ~ 1.5 ~ 
+             expression(phi ~ "="~ 9.5 ~ "," ~ nu ~ "=" ~ 1.5 ~ 
                           "," ~ delta^2 ~ "=" ~ 1.0))) 
 p1_2
 ggsave(paste0("./sim/pics/check_z_var_d2.png"), 
