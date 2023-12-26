66824fb784c95d1d6d1a4c5ee3e811cf022f3091
Lu Zhang <lzhang63@usc.edu>
2023-12-25 23:35:48 -0800

    add INLA test into the simulation study for prefix parameter choice

--- sim/projects/sim_cv_experiment_prefix.R
+++ sim/projects/sim_cv_experiment_prefix.R
@@ -143,18 +143,22 @@ weights_M_LP = matrix(0, length(deltasq_grid) * length(phi_grid) *
 #raw_data <- list() # record raw data
 expect_w <- list() # save the weighted latent process 
 expect_y <- list() # save the weighted prediction 
-DIV_matrix2 <- matrix(NA, nrow = N_list, ncol = 12)
+DIV_matrix2 <- matrix(NA, nrow = N_list, ncol = 18)
 #G: grid; E: empirical; P: posterior
 colnames(DIV_matrix2) <- c("SPE_stack_LSE_E", "SPE_stack_LP_E",
                           "SPE_stack_LSE_P", "SPE_stack_LP_P",
+                          "SPE_stack_LSE_I", "SPE_stack_LP_I",
                           "ELPD_stack_LSE_E", "ELPD_stack_LP_E",
                           "ELPD_stack_LSE_P", "ELPD_stack_LP_P",
+                          "ELPD_stack_LSE_I", "ELPD_stack_LP_I",
                           "SPE_w_stack_LSE_E", "SPE_w_stack_LP_E",
-                          "SPE_w_stack_LSE_P", "SPE_w_stack_LP_P")
+                          "SPE_w_stack_LSE_P", "SPE_w_stack_LP_P",
+                          "SPE_w_stack_LSE_I", "SPE_w_stack_LP_I")
 rownames(DIV_matrix2) <- paste(samplesize_ls) # check
-run_time2 <- matrix(0, 4, ncol = N_list)
+run_time2 <- matrix(0, 6, ncol = N_list)
 rownames(run_time2) <- c("Stack_LSE_E", "Stack_LP_E",
-                        "Stack_LSE_P", "Stack_LP_P")
+                        "Stack_LSE_P", "Stack_LP_P",
+                        "Stack_LSE_I", "Stack_LP_I")
 #MCMC_par <- list() # record the thinned MCMC chains for hyperparameters
 
 t <- proc.time()
@@ -308,9 +312,148 @@ for(r in 1:N_list){ # repeat
   DIV_matrix2[r, "ELPD_stack_LSE_P"] = mean(log(exp(lp_pred_grid) %*% CV_fit_LSE$wts))
   DIV_matrix2[r, "ELPD_stack_LP_P"] = mean(log(exp(lp_pred_grid) %*% CV_fit_LP$wts))
   
+  #########################################
+  ## select prefixed value based on INLA ##
+  #########################################
+  df = data.frame(y=c(raw_data[[r]]$y[raw_data[[r]]$ind_mod], rep(NA, 100)), 
+                  locx=raw_data[[r]]$coords[, 1], 
+                  locy=raw_data[[r]]$coords[, 2], x = raw_data[[r]]$X[, 2])
+  fake.locations = matrix(c(0,0,1,1, 0, 1, 1, 0), nrow = 4, byrow = T)
+  n <- length(raw_data[[r]]$coords[, 1]) - 100
+  mesh = inla.mesh.2d(loc = fake.locations, max.edge=c(2/sqrt(n), 10/sqrt(n)))
+  mesh$n
+  
+  A = inla.spde.make.A(mesh=mesh, loc=data.matrix(df[ , c('locx', 'locy')]))
+  prior.median.sd = 1; prior.median.range = 1/raw_data[[r]]$phi
+  
+  a1 <- 3/2
+  spde1 = inla.spde2.pcmatern(mesh, alpha = a1,
+                             prior.range = c(prior.median.range, .5), 
+                             prior.sigma = c(prior.median.sd, .5))
+  stack1 = inla.stack(tag='est',
+                     # - Name (nametag) of the stack
+                     # - Here: est for estimating
+                     data=list(y=df$y),
+                     effects=list(
+                       # - The Model Components
+                       s=1:spde1$n.spde, 
+                       # - The first is 's' (for spatial)
+                       data.frame(intercept=1, x=df$x)),
+                     # - The second is all fixed effects
+                     A=list(A, 1)
+  )
+  
+  family = "gaussian"
+  prior.median.sd.g = 1 # prior median for sigma.epsilon
+  control.family = list(hyper = list(prec = list(
+    prior = "pc.prec", param = c(prior.median.sd.g,0.5))))
+  
+  initial.theta = NULL #c(2.35, 0.79, 0.46)
+  
+  res1 = inla(y~x + f(s, model=spde1), data=inla.stack.data(stack1),
+             family = family,
+             control.family = control.family,
+             control.predictor=list(A = inla.stack.A(stack1)),
+             quantiles=c(0.01, 0.1, 0.5, 0.9, 0.99),
+             control.mode = list(restart = T, theta = initial.theta))
+  
+  a2 <- 2
+  spde2 = inla.spde2.pcmatern(mesh, alpha = a2,
+                             prior.range = c(prior.median.range, .5), 
+                             prior.sigma = c(prior.median.sd, .5))
+  stack2 = inla.stack(tag='est',
+                     # - Name (nametag) of the stack
+                     # - Here: est for estimating
+                     data=list(y=df$y),
+                     effects=list(
+                       # - The Model Components
+                       s=1:spde2$n.spde, 
+                       # - The first is 's' (for spatial)
+                       data.frame(intercept=1, x=df$x)),
+                     # - The second is all fixed effects
+                     A=list(A, 1)
+  )
+  
+  res2 = inla(y~x + f(s, model=spde2), data=inla.stack.data(stack2),
+             family = family,
+             control.family = control.family,
+             control.predictor=list(A = inla.stack.A(stack2)),
+             quantiles=c(0.01, 0.1, 0.5, 0.9, 0.99),
+             control.mode = list(restart = T, theta = initial.theta))
+  
+  all_prefix_ls <- cbind(
+    c(1/(exp(res1$joint.hyper$`Log precision for the Gaussian observations`)*
+         exp(res1$joint.hyper$`log(Stdev) for s`)^2),
+      1/(exp(res2$joint.hyper$`Log precision for the Gaussian observations`)*
+           exp(res2$joint.hyper$`log(Stdev) for s`)^2)) ,
+    c(1/exp(res1$joint.hyper$`log(Range) for s`), 
+      1/exp(res2$joint.hyper$`log(Range) for s`)),
+    c(rep(a1 - 1, length(res1$joint.hyper$`log(Range) for s`)), 
+      rep(a2 - 1, length(res2$joint.hyper$`log(Range) for s`))))
+  colnames(all_prefix_ls) <- c("delatsq", "phi", "nu")
+  all_prefix_ls <- as.data.frame(all_prefix_ls)
+  
+  CV_fit_LSE <- sp_stacking_K_fold3(
+    X = X[ind_mod, ], y = y[ind_mod], coords = coords[ind_mod, ],
+    all_prefix_ls = all_prefix_ls, priors = priors, 
+    K_fold = K_fold, seed = seed, label = "LSE")
+  #weights_M_LSE[, r] <- CV_fit_LSE$wts
+  run_time2["Stack_LSE_I", r] <- CV_fit_LSE$time[3]
+  
+  CV_fit_LP <- sp_stacking_K_fold3(
+    X = X[ind_mod, ], y = y[ind_mod], coords = coords[ind_mod, ],
+    all_prefix_ls = all_prefix_ls, 
+    priors = priors, K_fold = K_fold,
+    seed = seed, label = "LP", MC = FALSE)
+  #weights_M_LP[, r] <- CV_fit_LP$wts
+  run_time2["Stack_LP_I", r] <- CV_fit_LP$time[3]
   
   
+  ## stacking mean squared prediction error ##
+  y_pred_grid <- matrix(0, nrow = N_ho, ncol = nrow(CV_fit_LSE$grid_all))
+  w_expect_grid <- matrix(0, nrow = N, ncol = nrow(CV_fit_LSE$grid_all))
   
+  for (i in 1:nrow(CV_fit_LSE$grid_all)){
+    if( (CV_fit_LSE$wts[i]>0) | (CV_fit_LP$wts[i]>0)){
+      pred_grid <- Conj_predict(X.mod = X[ind_mod, ], y.mod = y[ind_mod],
+                                coords.mod = coords[ind_mod, ],
+                                deltasq_pick = CV_fit_LSE$grid_all$deltasq[i],
+                                phi_pick = CV_fit_LSE$grid_all$phi[i], 
+                                nu_pick = CV_fit_LSE$grid_all$nu[i],
+                                priors,
+                                X.ho = X[-ind_mod, ], 
+                                coords.ho = coords[-ind_mod, ])
+      y_pred_grid[, i] <- pred_grid$y_expect
+      w_expect_grid[, i] <- pred_grid$w_expect
+    }
+  }
+  y_pred_stack_LSE = y_pred_grid %*% CV_fit_LSE$wts
+  DIV_matrix2[r, "SPE_stack_LSE_I"] <- mean((y_pred_stack_LSE - y[-ind_mod])^2)
+  y_pred_stack_LP = y_pred_grid %*% CV_fit_LP$wts
+  DIV_matrix2[r, "SPE_stack_LP_I"] <- mean((y_pred_stack_LP - y[-ind_mod])^2)
+  
+  w_expect_stack_LSE = w_expect_grid %*% CV_fit_LSE$wts
+  DIV_matrix2[r, "SPE_w_stack_LSE_I"] <- mean((w_expect_stack_LSE - w)^2)
+  w_expect_stack_LP = w_expect_grid %*% CV_fit_LP$wts
+  DIV_matrix2[r, "SPE_w_stack_LP_I"] <- mean((w_expect_stack_LP - w)^2)
+  
+  
+  ## stacking Expected log pointwise predictive density ##
+  lp_pred_grid <- matrix(0, nrow = N_ho, ncol = nrow(CV_fit_LSE$grid_all))
+  for (i in 1:nrow(CV_fit_LSE$grid_all)){
+    if((CV_fit_LSE$wts[i] > 0) | (CV_fit_LP$wts[i] > 0)){
+      lp_pred_grid[, i] <- Conj_lpd(X.mod = X[ind_mod, ], y.mod = y[ind_mod], 
+                                    coords.mod = coords[ind_mod, ], 
+                                    deltasq_pick = CV_fit_LSE$grid_all$deltasq[i],
+                                    phi_pick = CV_fit_LSE$grid_all$phi[i], 
+                                    nu_pick = CV_fit_LSE$grid_all$nu[i],
+                                    priors, X.ho = X[-ind_mod, ], 
+                                    y.ho = y[-ind_mod], 
+                                    coords.ho = coords[-ind_mod, ], MC = FALSE)
+    }
+  }
+  DIV_matrix2[r, "ELPD_stack_LSE_I"] = mean(log(exp(lp_pred_grid) %*% CV_fit_LSE$wts))
+  DIV_matrix2[r, "ELPD_stack_LP_I"] = mean(log(exp(lp_pred_grid) %*% CV_fit_LP$wts))
   
 }
 proc.time() - t
